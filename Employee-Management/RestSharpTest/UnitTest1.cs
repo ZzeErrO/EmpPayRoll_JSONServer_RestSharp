3792ab894801a7d3023239fa2deeb7514f6d72c8
prashikjaware <prashikjaware123@gmail.com>
2021-04-21 19:43:41 +0530

    [Prashik]Added: Ability to add a new Employee to Json server

--- Employee-Management/RestSharpTest/UnitTest1.cs
+++ Employee-Management/RestSharpTest/UnitTest1.cs
@@ -36,14 +36,30 @@ namespace RestSharpTest
             //assert
             Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
             List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
-            Assert.AreEqual(4, dataResponse.Count);
+            Assert.AreEqual(5, dataResponse.Count);
             foreach (var item in dataResponse)
             {
                 System.Console.WriteLine("id: " + item.id + "Name: " + item.name + "Salary: " + item.Salary);
             }
         }
 
+        [TestMethod]
+        public void givenEmployee_OnPost_ShouldReturnAddedEmployee()
+        {
+            RestRequest request = new RestRequest("/employees", Method.POST);
+            JObject jObjectbody = new JObject();
+            jObjectbody.Add("name", "Clark");
+            jObjectbody.Add("Salary", "15000");
+            request.AddParameter("application/json", jObjectbody, ParameterType.RequestBody);
 
+            //act
+            IRestResponse response = client.Execute(request);
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Clark", dataResponse.name);
+            Assert.AreEqual(15000, dataResponse.Salary);
+
+        }
     }
 
 
