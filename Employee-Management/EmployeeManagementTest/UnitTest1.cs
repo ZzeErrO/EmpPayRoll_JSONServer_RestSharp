94000f41031628261b6627f9e382aa9d5e169f0c
prashikjaware <prashikjaware123@gmail.com>
2021-04-21 19:22:22 +0530

    [Prashik]Added: Ability to retrive all employees in json server using rest sharp

---
+++ Employee-Management/EmployeeManagementTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using EmployeeManagement;
+using EmployeeManagement.Model.SalaryModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeeManagementTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GivenSalaryDataAbleToUpdateSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryUpdateModel updateModel = new SalaryUpdateModel()
+            {
+                SalaryId = 1,
+                Month = "Jan",
+                EmployeeSalary = 1300,
+                EmployeeId = 1
+            };
+
+            int EmpSalary = salary.UpdateEmployeeSalary(updateModel);
+
+            Assert.AreEqual(updateModel.EmployeeSalary, EmpSalary);
+        }
+    }
+
+
+}
